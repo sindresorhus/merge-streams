4e2a3191a93d2bbbce51185f6f689c18badc5d9b
ehmicky <ehmicky@gmail.com>
2024-01-21 03:22:57 +0000

    Improve stream termination, highWaterMark, objectMode

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 import {PassThrough as PassThroughStream} from 'node:stream';
+import {finished} from 'node:stream/promises';
 
 export default function mergeStreams(streams) {
 	if (!Array.isArray(streams)) {
 		throw new TypeError(`Expected an array, got \`${typeof streams}\`.`);
 	}
 
-	const passThroughStream = new PassThroughStream({objectMode: true});
+	const invalidStream = streams.find(stream => typeof stream?.pipe !== 'function');
+	if (invalidStream !== undefined) {
+		throw new TypeError(`Expected a readable stream, got: \`${typeof invalidStream}\`.`);
+	}
+
+	const objectMode = streams.some(({readableObjectMode}) => readableObjectMode);
+	const readableHighWaterMark = getReadableHighWaterMark(streams, objectMode);
+	const passThroughStream = new PassThroughStream({objectMode, writableHighWaterMark: 0, readableHighWaterMark});
 	passThroughStream.setMaxListeners(Number.POSITIVE_INFINITY);
 
 	if (streams.length === 0) {
@@ -13,27 +21,36 @@ export default function mergeStreams(streams) {
 		return passThroughStream;
 	}
 
-	let activeStreams = streams.length;
-
 	for (const stream of streams) {
-		if (!(typeof stream?.pipe === 'function')) {
-			throw new TypeError(`Expected a stream, got: \`${typeof stream}\`.`);
-		}
-
 		stream.pipe(passThroughStream, {end: false});
-
-		stream.on('end', () => {
-			activeStreams--;
-
-			if (activeStreams === 0) {
-				passThroughStream.end();
-			}
-		});
-
-		stream.on('error', error => {
-			passThroughStream.emit('error', error);
-		});
 	}
+
+	endWhenStreamsDone(passThroughStream, streams);
 
 	return passThroughStream;
 }
+
+const getReadableHighWaterMark = (streams, objectMode) => {
+	if (streams.length === 0) {
+		return 0;
+	}
+
+	const highWaterMarks = streams
+		.filter(({readableObjectMode}) => readableObjectMode === objectMode)
+		.map(({readableHighWaterMark}) => readableHighWaterMark);
+	return Math.max(...highWaterMarks);
+};
+
+const endWhenStreamsDone = async (passThroughStream, streams) => {
+	try {
+		await Promise.all(streams.map(stream => finished(stream, {cleanup: true, readable: true, writable: false})));
+		passThroughStream.end();
+	} catch (error) {
+		// This is the error thrown by `finished()` on `stream.destroy()`
+		if (error?.code === 'ERR_STREAM_PREMATURE_CLOSE') {
+			passThroughStream.destroy();
+		} else {
+			passThroughStream.destroy(error);
+		}
+	}
+};
